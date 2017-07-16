e30f34e4b224b3e12ded9bea8acff253a2968a1c
agirbau <andreu.girbau@gmail.com>
2017-07-16 14:53:38 +0900

    Problem with error (not decreasing)

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ import matplotlib.pyplot as plt
 training_set_inputs = array([[0,0,1],[1,1,1],[1,0,1],[0,1,1]])
 training_set_outputs = array([[0,1,1,0]]).T
 
+# Todo: Every layer (which are objects) has 3 different functions: 1) Forward, Derivative respect to the input, derivate respect to paramaters
+# Forward, backward derivatives, gradients
+
 class NeuralNetwork():
     def __init__(self):
         # We want the same random seed to be able to compare results
@@ -99,17 +102,18 @@ class MultiLayerNeuralNetwork():
 		return sigmoid_derivative
 	
 	def think(self,input,weights):
-		sum_inputs = sum(dot(input,weights))
+		sum_inputs = dot(input,weights)
 		activations = self.__sigmoid(sum_inputs)
 		return activations
 
 	def backpropagation(self,input,output,error):
-		der_out = self.__sigmoid_derivative(output) * error
+		der_out = error * self.__sigmoid_derivative(output)
+		# print der_out
+		# print input.T
 		adjustment = dot(input.T, der_out)
 		return adjustment
 	
 	def train(self,training_set_inputs,training_set_outputs,n_iter):
-	
 		for i in xrange(n_iter):
 			# 1.Forward propagation
 			# Sum weights * inputs
@@ -117,28 +121,34 @@ class MultiLayerNeuralNetwork():
 			# activations_l1 = self.__sigmoid(sum_inputs_l1)
 			# print "Activations old: "
 			# print activations_l1
-			
+
 			activations_l1 = self.think(training_set_inputs,self.l1_synaptic_weights)			
 			# print "Activations: "
 			# print activations_l1
-			
+
 			# sum_inputs_l2 = sum(dot(activations_l1, self.l2_synaptic_weights))
 			# activations_l2 = self.__sigmoid(sum_inputs_l2)
 			# print activations_l2
-			
+
 			activations_l2 = self.think(activations_l1,self.l2_synaptic_weights)
-			
-			
+
+
 			# 2.Error computation
-			error = training_set_outputs - activations_l2
-			print error
-			
+			error = activations_l2 - training_set_outputs
+			# print error
+			if i % 100 == 0:
+				errors_abs = absolute(error)
+				errors_rep = sum(errors_abs)
+				print errors_rep       
+				plt.scatter(i/100,errors_rep)	
+				plt.pause(0.05)		
+
 			# 3.Backpropagation
-			print activations_l1
-			print activations_l2
+			# print activations_l1
+			# print activations_l2
 			adj_l1 = self.backpropagation(activations_l1,activations_l2,error)
-			print adj_l1
-			
+			# print adj_l1
+
 			adj = self.backpropagation(training_set_inputs,activations_l1,adj_l1)
 			# weight update
 			self.l2_synaptic_weights += adj_l1
@@ -159,16 +169,18 @@ if __name__ == "__main__":
 	print training_set_inputs
 	print training_set_outputs
 	
-	n_iter = 1
+	n_iter = 10000
 	neural_network.train(training_set_inputs,training_set_outputs,n_iter)
+	
+	
 	
 if __name__=="__L1__": #__main__
     #print "Hello world"
     # Initialize 1 neural network
     neural_network = NeuralNetwork()
     
-    print "Random starting synaptic weights: "
-    print neural_network.synaptic_weights
+    # print "Random starting synaptic weights: "
+    # print neural_network.synaptic_weights
     
     training_set_inputs = array([[0,0,1],[1,1,1],[1,0,1],[0,1,1]])
     training_set_outputs = array([[0,1,1,0]]).T
@@ -179,8 +191,8 @@ if __name__=="__L1__": #__main__
     neural_network.train(training_set_inputs,training_set_outputs,n_iter)
     plt.show()
 	
-    print "New synaptical weights"
-    print neural_network.synaptic_weights
+    # print "New synaptical weights"
+    # print neural_network.synaptic_weights
 
     test_input = array([[1,0,0]])
     res = neural_network.think(test_input)
